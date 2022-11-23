1a553b0d587b537f2a7e43f8067cdab4126f1906
n-ryu <ababc100564@gmail.com>
2022-11-23 20:00:58 +0900

    feat: 결과 파일 저장 경로 지정 및 기타 옵션 설정

---
+++ b.test.js
@@ -0,0 +1,9 @@
+describe("test B", () => {
+  it("1 is 1", () => {
+    expect(1).toBe(1);
+  });
+
+  it("1 is 1", () => {
+    expect(1).toBe(2);
+  });
+})
--- test.js
+++
@@ -1,7 +0,0 @@
-test("1 is 1", () => {
-  expect(1).toBe(1);
-});
-
-test("1 is 1", () => {
-  expect(1).toBe(2);
-});
